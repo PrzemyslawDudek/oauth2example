c75e6651083ec70b0f9a07920b67d345c100376f
Przemysław Dudek <dudek.mail@gmail.com>
2023-01-15 18:20:56 +0100

    Initial commit

---
+++ src/main/java/pl/dudecode/oauth2example/UserController.java
@@ -0,0 +1,30 @@
+package pl.dudecode.oauth2example;
+
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.Map;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/")
+    public String getUserDetails(Principal principal) {
+        OAuth2Authentication oAuth = (OAuth2Authentication) principal;
+        boolean isAuthenticated =  oAuth.isAuthenticated();
+
+        Map<String, Object> details = (Map<String, Object>) oAuth.getUserAuthentication().getDetails();
+        String username = (String) details.get("name");
+
+        return "Username: " + username + "<br><br> Is User Authenticated?: " + isAuthenticated;
+    }
+
+    @GetMapping("/user")
+    public Principal showUser(Principal p) {
+        System.out.println(p.getClass().getName());
+        return p;
+    }
+
+}
